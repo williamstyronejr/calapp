22f1b78ba3e4851f4629318ac2a74d404d024128
williamstyronejr <itenory@gmail.com>
2023-07-22 22:29:54 -0400

    Initial Commit

---
+++ CalendarApp/CalendarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarAppApp.swift
+//  CalendarApp
+//
+//  Created by Tyrone Williams on 7/22/23.
+//
+
+import SwiftUI
+
+@main
+struct CalendarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
